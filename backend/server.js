0e93d142606dbe96ad77d336b4949b13be93acb6
renancoelho1710 <renan.azulveiculos@gmail.com>
2025-12-25 21:01:42 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
